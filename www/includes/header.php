96099e18dd67529b22c856173058568686cba042
Tosin <tosinolugbenga@gmail.com>
2017-05-03 10:40:37 +0100

    acheive worked

--- www/includes/header.php
+++ www/includes/header.php
@@ -7,7 +7,7 @@
     <meta name="author" content="">
     <!--link rel="icon" href="../../favicon.ico"-->
 
-    <title>Blog Template for Bootstrap</title>
+    <title>SWAP SPACE BOX</title>
 
     <!-- Bootstrap core CSS -->
     <link href="styles/bootstrap.min.css" rel="stylesheet">
